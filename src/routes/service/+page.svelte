11d4d0954ca3d56560193a769d93eb9af63cd6b9
alberto-hong <yki.hong@navercorp.com>
2023-03-10 10:07:01 +0900

    remove console.log

--- src/routes/service/+page.svelte
+++ src/routes/service/+page.svelte
@@ -6,40 +6,76 @@
 		reader.readAsDataURL(image);
 		reader.onload = (event) => {
 			cloth = event.target.result;
-      console.log(cloth);
 		};
 	};
 </script>
 
 <h1>Service Page</h1>
 <div id="app">
-  <h1>Upload Image</h1>
-  {#if cloth}
-  <img class="cloth" src="{cloth}" alt="Uploaded Cloth Image" />
-  {:else}
-  <img class="cloth" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" alt="Example Cloth Image" /> 
-  {/if}
-  <img class="upload" src="https://static.thenounproject.com/png/625182-200.png" alt="" on:click={()=>{fileinput.click();}} />
-  <div class="chan" on:click={()=>{fileinput.click();}}>Choose Image</div>
-  <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
+	<h1>Upload Image</h1>
+	{#if cloth}
+		<img class="cloth" src={cloth} alt="" />
+	{:else}
+		<img
+			class="cloth"
+			src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png"
+			alt=""
+		/>
+	{/if}
+	<img
+		class="upload"
+		src="https://static.thenounproject.com/png/625182-200.png"
+		alt=""
+		on:click={() => {
+			fileinput.click();
+		}}
+		on:keydown={() => {
+			console.log('keydown');
+		}}
+		on:keyup={() => {
+			console.log('keyup');
+		}}
+	/>
+	<div
+		class="chan"
+		on:click={() => {
+			fileinput.click();
+		}}
+		on:keydown={() => {
+			console.log('keydown');
+		}}
+		on:keyup={() => {
+			console.log('keyup');
+		}}
+	>
+		Choose Image
+	</div>
+	<input
+		style="display:none"
+		type="file"
+		accept=".jpg, .jpeg, .png"
+		on:change={(e) => onFileSelected(e)}
+		bind:this={fileinput}
+	/>
 </div>
+
 <style>
-	#app{
-	display:flex;
-		align-items:center;
-		justify-content:center;
-		flex-flow:column;
-}
- 
-	.upload{
-		display:flex;
-	height:50px;
-		width:50px;
-		cursor:pointer;
+	#app {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-flow: column;
 	}
-	.cloth{
-		display:flex;
-		height:200px;
-		width:200px;
+
+	.upload {
+		display: flex;
+		height: 50px;
+		width: 50px;
+		cursor: pointer;
+	}
+	.cloth {
+		display: flex;
+		height: 200px;
+		width: 200px;
 	}
 </style>
