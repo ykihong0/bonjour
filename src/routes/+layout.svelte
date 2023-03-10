29c6413dc9d83666e6ca60b98e62077d6cf8aee0
alberto-hong <yki.hong@navercorp.com>
2023-03-10 10:01:28 +0900

    add image upload into service route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	<a href="/">Home</a>
 	<a href="/about">About</a>
 	<a href="/help">Help</a>
+	<a href="/product">Product</a>
+	<a href="/service">Service</a>
 </nav>
 <slot />
 
---
+++ src/routes/service/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+	let cloth, fileinput;
+	const onFileSelected = (event) => {
+		let image = event.target.files[0];
+		let reader = new FileReader();
+		reader.readAsDataURL(image);
+		reader.onload = (event) => {
+			cloth = event.target.result;
+      console.log(cloth);
+		};
+	};
+</script>
+
+<h1>Service Page</h1>
+<div id="app">
+  <h1>Upload Image</h1>
+  {#if cloth}
+  <img class="cloth" src="{cloth}" alt="Uploaded Cloth Image" />
+  {:else}
+  <img class="cloth" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" alt="Example Cloth Image" /> 
+  {/if}
+  <img class="upload" src="https://static.thenounproject.com/png/625182-200.png" alt="" on:click={()=>{fileinput.click();}} />
+  <div class="chan" on:click={()=>{fileinput.click();}}>Choose Image</div>
+  <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
+</div>
+<style>
+	#app{
+	display:flex;
+		align-items:center;
+		justify-content:center;
+		flex-flow:column;
+}
+ 
+	.upload{
+		display:flex;
+	height:50px;
+		width:50px;
+		cursor:pointer;
+	}
+	.cloth{
+		display:flex;
+		height:200px;
+		width:200px;
+	}
+</style>
